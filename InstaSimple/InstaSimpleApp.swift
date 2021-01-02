0f94c127cc0004f3f1c1597528e58d71c9b0053d
Gaurav Shah <gauravs7@berkeley.edu>
2021-01-01 20:54:10 -0800

    Initial Commit

---
+++ InstaSimple/InstaSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaSimpleApp.swift
+//  InstaSimple
+//
+//  Created by Gaurav Shah on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
